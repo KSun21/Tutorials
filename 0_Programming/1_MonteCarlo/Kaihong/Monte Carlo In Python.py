b193333dfcfaa076beeca7fc9e404ffc31c6c8cb
Kaihong Sun <89555427+KSun21@users.noreply.github.com>
2022-07-21 12:53:21 -0400

    Update Monte Carlo In Python.py

--- 0_Programming/1_MonteCarlo/Kaihong/Monte Carlo In Python.py
+++ 0_Programming/1_MonteCarlo/Kaihong/Monte Carlo In Python.py
@@ -6,7 +6,7 @@ count = 0
 for i in range(k):
     x = random.random()
     y = random.random()
-    if x**2 + y**2 < r**2:
+    if x**2 + y**2 <= r**2:
         count = count + 1
 pi = count/k * 4
 print(pi)
